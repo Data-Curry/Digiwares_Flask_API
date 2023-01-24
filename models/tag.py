864171fadd19857a2d84ba4d64c29134684eb9b4
Christopher Austin <66553323+Data-Curry@users.noreply.github.com>
2023-01-24 14:47:14 +0900

    Tag model given warehouse_id column.

--- models/tag.py
+++ models/tag.py
@@ -6,6 +6,7 @@ class TagModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
+    warehouse_id = db.Column(db.Integer, db.ForeignKey("warehouses.id"), nullable=False)
 
     warehouse = db.relationship("WarehouseModel", back_populates="tags")
     items = db.relationship("ItemModel", back_populates="tags", secondary="items_tags")
